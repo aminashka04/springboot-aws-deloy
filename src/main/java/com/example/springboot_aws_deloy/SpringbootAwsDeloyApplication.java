dce04e4700b27797fcf4b7b13dde2c8fba1b2b0d
aminashka <aminashka96@gmail.com>
2025-09-23 17:18:52 -0400

    first commit

---
+++ src/main/java/com/example/springboot_aws_deloy/SpringbootAwsDeloyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_aws_deloy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsDeloyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsDeloyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot_aws_deloy/SpringbootAwsDeloyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_aws_deloy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsDeloyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
